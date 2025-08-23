99da7872e06d4938b792c6162a79ab5b2a15271f
Brad <bradbdavis@yahoo.com>
2025-08-23 10:42:43 -0400

    fixes!

--- src/crtc.s
+++ src/crtc.s
@@ -1,19 +1,20 @@
-;RO  :  99 (0x63) - Nr of Horizontal Characters Total.
-;R1  :  80 (0x50) - Nr of Horizontal Characters Displayed.
-;R2  :  83 (0x52) - Horizontal Sync Position.
-;R3  :   12 (0x 0c) - Sync width.
-;R4  :  64 (0x1f) - Vertical Total.
-;R5  :   5 (0x d) - Vertical Total Adjustment.
-;R6  :  25 (0x1e) - Nr of Vertical Characters Displayed.
-;R7  :  61 (0x1f) - Vertical Sync Position (might need manual fine tuning).
-;R8  :   0 (0x 0) - Interlace Mode.
-;R9  :   7 (0x f) - Max Scanline Address.
-;R10 : 0 - Cursor Start Scan Line.  Should be 0?
-;R11 : 0 - Cursor Stop Scan Line.   Should be 0?
-;R12 :  0 - Start Address (High). Real start address is 0x0000.
-;R13 :  0 - Start Address (Low). Real start address is 0x0000.
-;R14 :  0 - Cursor Start Address (High). Cursor will be at position (0, 0).
-;R15 :  0 - Cursor Start Address (Low). Cursor will be at position (0, 0).
+;RO  : Nr of Horizontal Characters Total.
+;R1  : Nr of Horizontal Characters Displayed.
+;R2  : Horizontal Sync Position.
+;R3  : Sync width.
+;R4  : Vertical Total.
+;R5  : Vertical Total Adjustment.
+;R6  : Nr of Vertical Characters Displayed.
+;R7  : Vertical Sync Position (might need manual fine tuning).
+;R8  : Interlace Mode.
+;R9  : Max Scanline Address.
+;R10 : Cursor Start Scan Line.  Should be 0?
+;R11 : Cursor Stop Scan Line.   Should be 0?
+;R12 : Start Address (High). Real start address is 0x0000.
+;R13 : Start Address (Low). Real start address is 0x0000.
+;R14 : Cursor Start Address (High). Cursor will be at position (0, 0).
+;R15 : Cursor Start Address (Low). Cursor will be at position (0, 0).
+
 ;                    r0   r1   r2   r3   r4   r5   r6   r7   r8   r9   r10  r11  r12  r13  r14  r15
 CRTC_SETTINGS: .byte $31, $28, $29, $06, $1f, $0d, $19, $1d, $00, $0f, $60, $0f, $00, $00, $00, $00
 CRTC_CURSOR_H = $0E ; REG 14
@@ -31,37 +32,6 @@ COLOR_RAM_DEFAULT_VALUE = $1B
 LINE_NUM_CHARS = $28 ; 40 chars per line
 MAX_LINE = $19 ; 25 lines, so 24 is last line (0-24)
 
-; sets MC6845 cursor pos
-.macro _set_cursor_pos
-    LDA #CRTC_CURSOR_L
-    STA CRTC_ADDRESS
-    LDA CURSOR_ADDRESS_PTR
-    STA CRTC_REGISTER
-
-    LDA #CRTC_CURSOR_H
-    STA CRTC_ADDRESS
-    LDA CURSOR_ADDRESS_PTR+1
-    SEC
-    SBC #>CHAR_RAM
-    STA CRTC_REGISTER    
-.endmacro
-
-.macro _reset_vid_ptrs
-    ; init cursor address pointer to beginning of video ram
-    lda #<CHAR_RAM
-    sta CURSOR_ADDRESS_PTR
-    lda #>CHAR_RAM
-    sta CURSOR_ADDRESS_PTR+1
-
-    lda #$00
-    sta CURSOR_X_POS
-    sta CURSOR_Y_POS
-    sta VIDEO_SCROLL_POS
-    sta VIDEO_SCROLL_POS+1
-
-    _set_cursor_pos
-.endmacro
-
 ; Alters A, X, Y registers!
 INIT_CRTC:
     ldx #$00    ; crtc register address
@@ -74,10 +44,8 @@ INIT_CRTC:
     inx
     cpx #$10            ; decimal 16
     bne @settings_loop
-
-; Now clear char and color ram
+    ; Now clear char and color ram
     jsr VIDEO_CLEAR
-
     rts
 
 VIDEO_CLEAR:
@@ -109,7 +77,29 @@ VIDEO_CLEAR:
     sta SCRATCH_ADDR_RAM+1
     jsr @video_clear_ram    
 
-    _reset_vid_ptrs
+    lda #<CHAR_RAM
+    sta CURSOR_ADDRESS_PTR
+    lda #>CHAR_RAM
+    sta CURSOR_ADDRESS_PTR+1
+
+    lda #$00
+    sta CURSOR_X_POS
+    sta CURSOR_Y_POS
+    sta VIDEO_SCROLL_POS
+    sta VIDEO_SCROLL_POS+1
+
+    ; sets MC6845 cursor pos
+    LDA #CRTC_CURSOR_L
+    STA CRTC_ADDRESS
+    LDA CURSOR_ADDRESS_PTR
+    STA CRTC_REGISTER
+
+    LDA #CRTC_CURSOR_H
+    STA CRTC_ADDRESS
+    LDA CURSOR_ADDRESS_PTR+1
+    SEC
+    SBC #>CHAR_RAM
+    STA CRTC_REGISTER    
 
     ply
     pla
@@ -141,30 +131,46 @@ VIDEO_CLEAR:
 @clear_done:
     rts
 
-; SCRATCH_ADDR_RAM should have cursor position 
-SET_CURSOR_POSITION:
+; A contains a positive number of bytes to add to current position
+; Since we are only adding 1 byte, increase can only be 256 or less
+MOVE_CURSOR_POSITION:
+    PHX
+    PHA
     LDA #CRTC_CURSOR_L
     STA CRTC_ADDRESS
-    LDA SCRATCH_ADDR_RAM
-    STA CRTC_REGISTER
+    ; LDA CRTC_REGISTER
+    PLA
+    CLC
+    ADC CRTC_REGISTER
+    BCC @no_rollover
 
-    LDA #CRTC_CURSOR_H
-    STA CRTC_ADDRESS
-    LDA SCRATCH_ADDR_RAM+1
+    LDX #CRTC_CURSOR_H
+    STX CRTC_ADDRESS
+    LDX CRTC_REGISTER
+    INX
+    STX CRTC_REGISTER
+
+    LDX #CRTC_CURSOR_L
+    STX CRTC_ADDRESS
+@no_rollover:
     STA CRTC_REGISTER
+    PLX
     RTS
 
-; Loads current cursor address and places in SCRATCH_ADDR_RAM
-GET_CURSOR_POSITION:
+CURSOR_BACKSPACE:
     LDA #CRTC_CURSOR_L
     STA CRTC_ADDRESS
     LDA CRTC_REGISTER
-    STA SCRATCH_ADDR_RAM
-
+    DEC
+    STA CRTC_REGISTER
+    CMP #$FF
+    BNE @no_rollover
     LDA #CRTC_CURSOR_H
     STA CRTC_ADDRESS
     LDA CRTC_REGISTER
-    STA SCRATCH_ADDR_RAM+1
+    DEC
+    STA CRTC_REGISTER
+@no_rollover:
     RTS
 
 ; clear current line
@@ -190,8 +196,18 @@ VIDEO_CLEAR_LINE:
 @exit:
     rts
 
-; 2048 bytes is memory size
-; 2040 is last position before wrap around, 7f8
+SET_SCROLL:
+    lda #CRTC_START_L
+    sta CRTC_ADDRESS
+    lda VIDEO_SCROLL_POS
+    sta CRTC_REGISTER
+
+    lda #CRTC_START_H
+    sta CRTC_ADDRESS
+    lda VIDEO_SCROLL_POS+1
+    sta CRTC_REGISTER
+    rts
+
 VIDEO_SCROLL_UP:
     lda VIDEO_SCROLL_POS
     clc
@@ -200,17 +216,6 @@ VIDEO_SCROLL_UP:
     inc VIDEO_SCROLL_POS+1
 @no_scroll_carry:
     sta VIDEO_SCROLL_POS
-    
-    LDA VIDEO_SCROLL_POS+1
-    CMP #$07
-    BNE @exit
-    LDA VIDEO_SCROLL_POS
-    CMP #$D0
-    BNE @exit
-    LDA #$00
-    STA VIDEO_SCROLL_POS
-    STA VIDEO_SCROLL_POS+1
-@exit:
     rts
 
 ; A - register contains ascii character code to write to memory
@@ -240,6 +245,10 @@ VIDEO_WRITE_CHAR:
     lda #LINE_NUM_CHARS          ; load with 40 (or 40-1)
     SEC
     sbc CURSOR_X_POS    ; subtract cursor x position from 40, tells us how many to add to get to next line
+    PHA
+    ; A now contains how many bytes to add to get to next line
+    JSR MOVE_CURSOR_POSITION
+    PLA
     clc
     adc CURSOR_ADDRESS_PTR ; add value in low byte memory ptr to the amount we need to add
     bcc @no_low_255_rollover
@@ -257,6 +266,10 @@ VIDEO_WRITE_CHAR:
 @enter_key_not_pressed:
     sta (CURSOR_ADDRESS_PTR)
     inc CURSOR_ADDRESS_PTR  ;increment cursor video memory position
+
+    LDA #$01
+    JSR MOVE_CURSOR_POSITION
+
     inc CURSOR_X_POS        ;increment our x char position tracker
     ldx CURSOR_X_POS        ; check if we are going to position 40 (next line so 0)
     cpx #LINE_NUM_CHARS                ; are we at char position 40?
@@ -283,8 +296,6 @@ VIDEO_WRITE_CHAR:
     cpx #<CHAR_RAM_END
     bcc @exit_video_write_char
 
-    LDA #$71
-    STA $9900
     SEC
     LDA CURSOR_ADDRESS_PTR
     SBC #<CHAR_RAM_END
@@ -294,24 +305,18 @@ VIDEO_WRITE_CHAR:
     CLC
     ADC #>CHAR_RAM
     STA CURSOR_ADDRESS_PTR+1
-    ; ldx #<CHAR_RAM
-    ; stx CURSOR_ADDRESS_PTR
-    ; ldx #>CHAR_RAM
-    ; stx CURSOR_ADDRESS_PTR+1
-    ; ldx #$00
-    ; stx CURSOR_X_POS
+
 @exit_video_write_char:
     lda SCRATCH_DATA_RAM
     cmp #$00
     beq @exit
-    ; jsr VIDEO_CLEAR_LINE
+    jsr VIDEO_CLEAR_LINE
     ; do we need to adjust the vertical scrolling?
     lda CURSOR_Y_POS
     cmp #MAX_LINE
     bne @exit
     jsr VIDEO_SCROLL_UP
 @exit:
-    _set_cursor_pos
     jsr SET_SCROLL
     PLX
     PLA
@@ -330,9 +335,7 @@ VIDEO_WRITE_CHAR:
     bne @video_cursor_backspace_exit
     dec CURSOR_ADDRESS_PTR+1    
 @video_cursor_backspace_exit:
-    ; erase value at that location
-    lda #CHAR_RAM_DEFAULT_VALUE
-    sta (CURSOR_ADDRESS_PTR)
-    _set_cursor_pos
+    LDA #-1
+    JSR CURSOR_BACKSPACE
     plx
     rts
