7fb1a6914c9ecda6856e1ecdb52a659e03753895
Brad Davis <bradbdavis@yahoo.com>
2025-08-25 07:01:53 -0400

    add new zero page

--- src/crtc.s
+++ src/crtc.s
@@ -56,9 +56,9 @@ VIDEO_CLEAR:
     lda #CHAR_RAM_DEFAULT_VALUE
     sta SCRATCH_DATA_RAM
     lda #<CHAR_RAM
-    sta CURSOR_ADDRESS_PTR
+    sta VIDEO_CHAR_PTR
     lda #>CHAR_RAM
-    sta CURSOR_ADDRESS_PTR+1
+    sta VIDEO_CHAR_PTR+1
     lda #<CHAR_RAM_END
     sta SCRATCH_ADDR_RAM
     lda #>CHAR_RAM_END
@@ -68,19 +68,25 @@ VIDEO_CLEAR:
     lda #COLOR_RAM_DEFAULT_VALUE
     sta SCRATCH_DATA_RAM
     lda #<COLOR_RAM
-    sta CURSOR_ADDRESS_PTR
+    sta VIDEO_CHAR_PTR
     lda #>COLOR_RAM
-    sta CURSOR_ADDRESS_PTR+1
+    sta VIDEO_CHAR_PTR+1
     lda #<COLOR_RAM_END
     sta SCRATCH_ADDR_RAM
     lda #>COLOR_RAM_END
     sta SCRATCH_ADDR_RAM+1
     jsr @video_clear_ram    
 
+    ; init data vars
     lda #<CHAR_RAM
-    sta CURSOR_ADDRESS_PTR
+    sta VIDEO_CHAR_PTR
     lda #>CHAR_RAM
-    sta CURSOR_ADDRESS_PTR+1
+    sta VIDEO_CHAR_PTR+1
+
+    lda #<COLOR_RAM
+    sta VIDEO_COLOR_PTR
+    lda #>COLOR_RAM
+    sta VIDEO_COLOR_PTR+1
 
     lda #$00
     sta CURSOR_X_POS
@@ -106,25 +112,25 @@ VIDEO_CLEAR:
 
 ; Uses A, Y regs
 ; Also uses 
-;   CURSOR_ADDRESS_PTR - used to calculate address to clear
+;   VIDEO_CHAR_PTR - used to calculate address to clear
 ;   SCRATCH_ADDR_RAM - end address to clear to
 ;   SCRATCH_DATA_RAM - value to set ram to 
 @video_clear_ram:
     lda SCRATCH_DATA_RAM
 @loop:
-    sta (CURSOR_ADDRESS_PTR)
-    inc CURSOR_ADDRESS_PTR
+    sta (VIDEO_CHAR_PTR)
+    inc VIDEO_CHAR_PTR
     beq @inc_high_byte
 @check_if_done:
-    ldy CURSOR_ADDRESS_PTR+1
+    ldy VIDEO_CHAR_PTR+1
     cpy SCRATCH_ADDR_RAM+1
     bne @loop
-    ldy CURSOR_ADDRESS_PTR
+    ldy VIDEO_CHAR_PTR
     cpy SCRATCH_ADDR_RAM
     beq @clear_done
     jmp @loop
 @inc_high_byte:
-    inc CURSOR_ADDRESS_PTR+1
+    inc VIDEO_CHAR_PTR+1
     jmp @check_if_done
 @clear_done:
     rts
@@ -176,9 +182,9 @@ CURSOR_BACKSPACE:
 ; assumes cursor address is at beginning of the line
 VIDEO_CLEAR_LINE:
     PHY
-    lda CURSOR_ADDRESS_PTR
+    lda VIDEO_CHAR_PTR
     sta SCRATCH_ADDR_RAM
-    lda CURSOR_ADDRESS_PTR+1
+    lda VIDEO_CHAR_PTR+1
     sta SCRATCH_ADDR_RAM+1
     ldx #$00
     lda #CHAR_RAM_DEFAULT_VALUE
@@ -239,18 +245,15 @@ VIDEO_WRITE_CHAR:
     bne @backspace_not_pressed
     jmp @video_cursor_backspace
 @backspace_not_pressed:
-    PHA
+    PHA ; preserve caller's state
     PHX
-    ; set 0 to scratch data, 1 indicates clear current line
-    ldx #$00
-    stx SCRATCH_DATA_RAM
+    PHY
+    LDY #$00 ; set 0 to scratch data, 1 indicates clear current line
     ; check if it's the enter key
     cmp #$0D
     bne @enter_key_not_pressed
 ; here enter key was pressed...
-    ldx #$01
-    stx SCRATCH_DATA_RAM ; indicate to clear current line and check for scroll
-
+    LDY #$01 ; indicate to clear newline and check for scroll
     ; determine how many bytes to add to get to next line on display...
     lda #LINE_NUM_CHARS          ; load with 40 (or 40-1)
     SEC
@@ -260,11 +263,11 @@ VIDEO_WRITE_CHAR:
     JSR MOVE_CURSOR_POSITION
     PLA
     clc
-    adc CURSOR_ADDRESS_PTR ; add value in low byte memory ptr to the amount we need to add
+    adc VIDEO_CHAR_PTR ; add value in low byte memory ptr to the amount we need to add
     bcc @no_low_255_rollover
-    inc CURSOR_ADDRESS_PTR+1 ; we rolled over 255 on low byte
+    inc VIDEO_CHAR_PTR+1 ; we rolled over 255 on low byte
 @no_low_255_rollover:
-    sta CURSOR_ADDRESS_PTR
+    sta VIDEO_CHAR_PTR
     lda #$00
     sta CURSOR_X_POS
     lda CURSOR_Y_POS
@@ -274,51 +277,45 @@ VIDEO_WRITE_CHAR:
 @dont_inc_y:
     jmp @check_at_max_video_ram
 @enter_key_not_pressed:
-    sta (CURSOR_ADDRESS_PTR)
-    inc CURSOR_ADDRESS_PTR  ;increment cursor video memory position
-
+    sta (VIDEO_CHAR_PTR)
+    inc VIDEO_CHAR_PTR  ;increment cursor video memory position
     LDA #$01
     JSR MOVE_CURSOR_POSITION
-
     inc CURSOR_X_POS        ;increment our x char position tracker
     ldx CURSOR_X_POS        ; check if we are going to position 40 (next line so 0)
     cpx #LINE_NUM_CHARS                ; are we at char position 40?
     bne @check_256_vid_rollover ; no, skip ahead
     ldx #$00                    ; yes, reset it to 0
+    LDY #$01 ; indicate to clear current line
     stx CURSOR_X_POS
     lda CURSOR_Y_POS
     cmp #MAX_LINE
-    beq @dont_inc_y2
+    beq @check_256_vid_rollover ; Don't inc y cursor, already at bottom of screen
     inc CURSOR_Y_POS
-@dont_inc_y2:
-    ldx #$01
-    stx SCRATCH_DATA_RAM ; indicate to clear current line
 @check_256_vid_rollover:
-    ldx CURSOR_ADDRESS_PTR
+    ldx VIDEO_CHAR_PTR
     cpx #$00
     bne @check_at_max_video_ram
-    inc CURSOR_ADDRESS_PTR+1
+    inc VIDEO_CHAR_PTR+1
 @check_at_max_video_ram:
-    ldx CURSOR_ADDRESS_PTR+1
+    ldx VIDEO_CHAR_PTR+1
     cpx #>CHAR_RAM_END
     bcc @exit_video_write_char
-    ldx CURSOR_ADDRESS_PTR
+    ldx VIDEO_CHAR_PTR
     cpx #<CHAR_RAM_END
     bcc @exit_video_write_char
     SEC
-    LDA CURSOR_ADDRESS_PTR
+    LDA VIDEO_CHAR_PTR
     SBC #<CHAR_RAM_END
-    STA CURSOR_ADDRESS_PTR
-    LDA CURSOR_ADDRESS_PTR+1
+    STA VIDEO_CHAR_PTR
+    LDA VIDEO_CHAR_PTR+1
     SBC #>CHAR_RAM_END
     CLC
     ADC #>CHAR_RAM
-    STA CURSOR_ADDRESS_PTR+1
-
+    STA VIDEO_CHAR_PTR+1
 @exit_video_write_char:
-    lda SCRATCH_DATA_RAM
-    cmp #$00
-    beq @exit
+    cpy #$00 ; do we need to clear line and check scroll?
+    beq @exit ; no, just exit
     jsr VIDEO_CLEAR_LINE
     ; do we need to adjust the vertical scrolling?
     lda CURSOR_Y_POS
@@ -326,8 +323,10 @@ VIDEO_WRITE_CHAR:
     bne @exit
     jsr VIDEO_SCROLL_UP
 @exit:
+    PLY
     PLX
     PLA
+
     rts
 
 ; back the cursor up 1 byte in the video ram
@@ -336,12 +335,12 @@ VIDEO_WRITE_CHAR:
     ldx CURSOR_X_POS
     cpx #$00
     beq @video_cursor_backspace_exit
-    dec CURSOR_ADDRESS_PTR
+    dec VIDEO_CHAR_PTR
     dec CURSOR_X_POS
-    ldx CURSOR_ADDRESS_PTR
+    ldx VIDEO_CHAR_PTR
     cpx #$ff
     bne @video_cursor_backspace_exit
-    dec CURSOR_ADDRESS_PTR+1    
+    dec VIDEO_CHAR_PTR+1    
 @video_cursor_backspace_exit:
     LDA #-1
     JSR CURSOR_BACKSPACE
